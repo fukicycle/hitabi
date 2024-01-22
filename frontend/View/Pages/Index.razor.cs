e11333a3c9dc700d6e54b03c76df4e4bf441e813
fukicycle <lego.sato.4135@gmail.com>
2024-01-22 16:40:39 +0900

    時間が長いので変更

--- frontend/View/Pages/Index.razor.cs
+++ frontend/View/Pages/Index.razor.cs
@@ -8,10 +8,10 @@ namespace frontend.View.Pages
         {
             await RunWithErrorHandling(async () =>
             {
-                await Task.Delay(1000);
+                await Task.Delay(4000);
                 //exception test
                 throw new Exception("この例外はテストの例外です。");
-            });
+            }, true);
         }
     }
 }
