3cc98788830999fd06fe51d012b956a4d39c5e73
fukicycle <lego.sato.4135@gmail.com>
2024-01-22 16:12:35 +0900

    Add state container

---
+++ frontend/Store/StateContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace frontend.Store
+{
+    public class StateContainer
+    {
+        public Action? OnChange { get; set; }
+
+        private void NotifyChanged()
+        {
+            OnChange?.Invoke();
+        }
+
+        //state management property
+    }
+}
