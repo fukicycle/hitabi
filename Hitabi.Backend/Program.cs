0201956016cd2a189c85413406166047443b79a8
Fuki Sato <lego.sato.4135@gmail.com>
2024-03-16 17:48:06 +0900

    CORSの設定

--- Hitabi.Backend/Program.cs
+++ Hitabi.Backend/Program.cs
@@ -1,4 +1,23 @@
+using System.Security.Cryptography;
+using Hitabi.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
+//CORSの設定
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7175", "https://fukicycle.github.io")
+                .WithMethods("GET", "POST", "OPTIONS", "DELETE")
+                .WithHeaders("Authorizaion", "Content-Type")
+                .AllowCredentials();
+    });
+});
+
+//JWTの設定
+//JWTキーの作成
+RandomNumberGenerator.Fill(ApplicationSettings.JWT_KEY);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
