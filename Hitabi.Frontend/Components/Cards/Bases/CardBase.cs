b66c34ab297a27825304e54ee545754c201a1b53
WaraZee <moto_vr_46@yahoo.co.jp>
2024-03-17 17:42:14 +0900

    edited card components

---
+++ Hitabi.Frontend/Components/Cards/Bases/CardBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Hitabi.Frontend.Components.Cards.Bases
+{
+    public abstract class CardBase : ComponentBase
+    {
+        /// <summary>
+        /// 説明に表示される文字を指定
+        /// </summary>
+        [Parameter]
+        public string Content { get; set; } = "Content";
+    }
+}
