bdd8792ddef2b0a83d89e6203e16ac807212edbe
WaraZee <moto_vr_46@yahoo.co.jp>
2024-03-18 22:57:12 +0900

    edited CardHeader component

---
+++ Hitabi.Frontend/Components/Cards/CardHeader.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Hitabi.Frontend.Components.Cards
+{
+    public partial class CardHeader : ComponentBase
+    {
+        /// <summary>
+        /// 都道府県を指定
+        /// </summary>
+        [Parameter]
+        public string Prefecture { get; set; } = "Prefecture";
+        /// <summary>
+        /// 月を指定
+        /// </summary>
+        [Parameter]
+        public string Month { get; set; } = "Month";
+        /// <summary>
+        /// 宿泊数を指定
+        /// </summary>
+        [Parameter]
+        public string NumberOfStay { get; set; } = "NumberOfStay";
+    }
+}
