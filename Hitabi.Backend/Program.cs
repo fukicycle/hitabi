9d815e8b45f5de8f2ba470a777332dec57d34b90
Fuki Sato <lego.sato.4135@gmail.com>
2024-03-20 22:44:35 +0900

    Add jwt bearer authentication

--- Hitabi.Backend/Program.cs
+++ Hitabi.Backend/Program.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography;
 using Hitabi.Shared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 //CORSの設定
@@ -18,8 +20,31 @@ builder.Services.AddCors(options =>
 //JWTキーの作成
 RandomNumberGenerator.Fill(ApplicationSettings.JWT_KEY);
 
+//JWT認証の設定
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "sato-home.mydns.jp",
+        ValidAudience = "fukicycle.github.io",
+        IssuerSigningKey = new SymmetricSecurityKey(ApplicationSettings.JWT_KEY),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//CORS
+app.UseCors();
+
+//JWT
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
