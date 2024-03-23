d23a575b7d1a0074de88849b650cd81f79281959
WaraZee <moto_vr_46@yahoo.co.jp>
2024-03-23 15:37:36 +0900

    added PlanCalendar.razor

---
+++ Hitabi.Frontend/Components/Calendars/Bases/CalendarBase.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Hitabi.Frontend.Components.Calendars.Bases
+{
+    public abstract class CalendarBase : ComponentBase
+    {
+        /// <summary>
+        /// 日付を指定
+        /// </summary>
+        [Parameter]
+        public string Day { get; set; } = "1";
+    }
+}
