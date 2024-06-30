2825b83d33f5e83b5202ac31f1353f99eca71d9a
Fuki Sato <lego.sato.4135@gmail.com>
2024-06-30 11:46:32 +0900

    JWT認証を削除

--- Hitabi.Shared/ApplicationSettings.cs
+++ Hitabi.Shared/ApplicationSettings.cs
@@ -3,6 +3,5 @@
     public static class ApplicationSettings
     {
         public static ApplicationMode ApplicationMode = ApplicationMode.Dev;
-        public static byte[] JWT_KEY = new byte[64];
     }
 }
