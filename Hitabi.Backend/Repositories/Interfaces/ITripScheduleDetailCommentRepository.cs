1ab727c6150c002cc069797c8ede3e19e19e5c82
ahiru-senpai <k.kinbara1016@gmail.com>
2024-08-03 17:08:24 +0900

    IMemberRepositry作成

---
+++ Hitabi.Backend/Repositories/Interfaces/ITripScheduleDetailCommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hitabi.Shared.Models;
+
+namespace Hitabi.Backend.Repositories.Interfaces
+{
+    public interface ITripScheduleDetailCommentRepository
+    {
+        IList<TripScheduleDetailComment> GetTripScheduleDetailComments();
+
+        TripScheduleDetailComment GetTripScheduleDetailCommentByGuid(Guid guid);
+
+        TripScheduleDetailComment CreateTripScheduleDetailComment();
+
+        bool UpdateTripScheduleDetailComment();
+    }
+}
---
+++ Hitabi.Backend/Repositories/Interfaces/ITripScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hitabi.Shared.Models;
+
+namespace Hitabi.Backend.Repositories.Interfaces
+{
+    public interface ITripScheduleRepository
+    {
+        IList<TripSchedule> GetTripSchedules();
+
+        TripSchedule GetTripScheduleByGuid(Guid guid);
+
+        TripSchedule GetTripSchedule();
+
+        bool UpdateTripSchedule();
+    }
+}
