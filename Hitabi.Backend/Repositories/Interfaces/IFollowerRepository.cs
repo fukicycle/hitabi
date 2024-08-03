38a66893f475d4a6139e155be0bd07c0999304f2
ahiru-senpai <k.kinbara1016@gmail.com>
2024-08-03 17:04:16 +0900

    FollowerRepositry作成

---
+++ Hitabi.Backend/Repositories/Interfaces/IFollowerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hitabi.Shared.Models;
+
+namespace Hitabi.Backend.Repositories.Interfaces
+{
+    public interface IFollowerRepository
+    {
+        IList<Follower> GetFollowers();
+
+        Follower GetFollowerByGuid(Guid id);
+
+        Follower CreateFollower();
+
+        bool UpdateFollower();
+    }
+}
