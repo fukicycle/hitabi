4ba11317904e9bd9d55b9043d38ff6b63e246cd4
shogo16212 <shogo16212@gmail.com>
2024-08-18 20:17:16 +0900

    IMemberBudgetRepository追加

---
+++ Hitabi.Backend/Repositories/Interfaces/IMemberBudgetRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hitabi.Shared.Models;
+
+namespace Hitabi.Backend.Repositories.Interfaces
+{
+    public interface IMemberBudgetRepository
+    {
+        IList<MemberBudget> GetMemberBudgets();
+
+        MemberBudget GetMemberBudgetByGuid(Guid guid);
+
+        MemberBudget CreateMemberBudget();
+
+        bool UpdateMemberBudget();
+    }
+}
