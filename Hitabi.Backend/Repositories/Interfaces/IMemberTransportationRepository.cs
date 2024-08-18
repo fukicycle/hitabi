b1ded7f5d43691a426354a48cb0bd53f2c6fe9a3
shogo16212 <shogo16212@gmail.com>
2024-08-18 20:24:59 +0900

    IMemberTransportationRepository追加

---
+++ Hitabi.Backend/Repositories/Interfaces/IMemberTransportationRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hitabi.Shared.Models;
+
+namespace Hitabi.Backend.Repositories.Interfaces
+{
+    public interface IMemberTransportationRepository
+    {
+        IList<MemberTransportation> GetMemberTransportations();
+
+        MemberTransportation GetMemberTransportationByGuid(Guid guid);
+
+        MemberTransportation CreateTransportation();
+
+        bool UpdateTransportation();
+    }
+}
